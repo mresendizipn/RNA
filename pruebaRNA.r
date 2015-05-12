40218ac026e3e93b88be95f431e438124c658078
Jose Miguel <mresendizipn@gmail.com>
2015-05-12 16:23:16 -0500

    prueba

--- pruebaRNA.r
+++ pruebaRNA.r
@@ -1,11 +1,16 @@
-MLP<-list(R=2,S=c(3,4,1), funAct = c("tansig","pureline","pureline"))
-funcionesActivacion<-c("pureline","logsig","tansig")
-funcionesActivacion<-c("purelineDer","logsigDer","tansigDer")
+source('~/.active-rstudio-document')
+
+
+MLP<-list(R=2,S=c(3,4,1), funAct = c("logsig","pureline","tansig"))
 
 RNA <- createObjRNA(MLP)
 
 a0<-matrix(c(3,7),ncol = 1)
-propagationAdelante(RNA,a0) 
+at <- matrix(c(2),ncol = 1)
+eps<-0.8
+
+RNA <- propagationAdelante(RNA,a0) 
+RNA <- PropagacionAtras(RNA,at,a0,eps)
 
 data<-matrix(c(3,1,5,2,2,4),ncol = 2,byrow =T) 
 target <- matrix(c(1,1,0),ncol = 1)
--- rna.r
+++ rna.r
@@ -1,35 +1,90 @@
+funcionesActivacion<-c("pureline","logsig","tansig")
+funcionesActivacion<-c("purelineDer","logsigDer","tansigDer")
+
+
+PropagacionAtras <- function(RNA,at,a0,eps){
+ 
+  ##Calculate Matriz S
+    namesS <- paste(rep("w",length(MLP$S)),as.character(1:(length(MLP$S))), sep = "")
+    S <- vector("list", length(MLP$S))
+    names(S) <- namesS
+  
+    for(i in length(RNA$S):1 ){
+      
+      if(i == length(RNA$S)){
+        S[[i]] <- -2 * (diag(evalFunc(RNA$Nn[[i]],RNA$Sensibilidad[[i]][1])) %*%  (at-RNA$An[[i]]) )
+      }else{
+        S[[i]] <- diag(as.vector(evalFunc(RNA$Nn[[i]],RNA$Sensibilidad[[i]][1]))) %*% t(RNA$Wn[[i+1]]) %*% S[[i+1]]
+      }
+    }
+    
+  ##Update Wn and Bn
+    for(i in 1:length(RNA$Wn)){
+      if(i == 1){
+        RNA$Wn[[i]] = RNA$Wn[[i]] - eps*(S[[i]] %*% t(a0))
+        
+      }else{
+        RNA$Wn[[i]] = RNA$Wn[[i]] - eps*(S[[i]] %*% t(RNA$An[[i-1]])) 
+      }
+      RNA$Bn[[i]] <- RNA$Bn[[i]] - eps*(S[[i]])
+    }
+    RNA
+}
+
+
 createObjRNA <- function(MLP){
   ##Create list W
-  namesWn <- paste(rep("w",length(MLP$S)),as.character(1:(length(MLP$S))), sep = "")
-  Wn <- vector("list", length(MLP$S))
-  names(Wn) <- namesWn
-  
+    namesWn <- paste(rep("w",length(MLP$S)),as.character(1:(length(MLP$S))), sep = "")
+    Wn <- vector("list", length(MLP$S))
+    names(Wn) <- namesWn
+    
   ##Create list B
-  namesBn <- paste(rep("b",length(MLP$S)),as.character(1:(length(MLP$S))), sep = "")
-  Bn <- vector("list", length(MLP$S))
-  names(Bn) <- namesBn
+    namesBn <- paste(rep("b",length(MLP$S)),as.character(1:(length(MLP$S))), sep = "")
+    Bn <- vector("list", length(MLP$S))
+    names(Bn) <- namesBn
  
   ##Create list A
-  namesAn <- paste(rep("a",length(MLP$S)),as.character(1:(length(MLP$S))), sep = "")
-  An <- vector("list", length(MLP$S))
-  names(An) <- namesAn
+    namesAn <- paste(rep("a",length(MLP$S)),as.character(1:(length(MLP$S))), sep = "")
+    An <- vector("list", length(MLP$S))
+    names(An) <- namesAn
+  
+  ##Create list N
+  namesNn <- paste(rep("n",length(MLP$S)),as.character(1:(length(MLP$S))), sep = "")
+  Nn <- vector("list", length(MLP$S))
+  names(Nn) <- namesNn
 
+  #sensibilidad 
+    namesSensibilidad <- paste(rep("S",length(MLP$S)),as.character(1:(length(MLP$S))), sep = "")
+    Sensibilidad <- vector("list", length(MLP$S))
+    names(Sensibilidad) <- namesSensibilidad
+  
   ## Load randon values
   rnaConf<-c(MLP$R,MLP$S)
   for(i in 1:length(MLP$S) ){
     Wn[[i]] <- matrix(rnorm(rnaConf[i]*rnaConf[i+1]),nrow = MLP$S[i])
     Bn[[i]] <- matrix((rep(1,rnaConf[i+1])),ncol = 1)
+    Sensibilidad[[i]] <- createMatrixSensitive(MLP$S[i],MLP$funAct[i])
   }
   #create list with vector of RNA
-  list(Wn=Wn, Bn = Bn,An=An, funAct = MLP$funAct) 
+  list(Wn=Wn, Bn = Bn,An=An,Nn= Nn, funAct = MLP$funAct,Sensibilidad=Sensibilidad ) 
+}
+
+createMatrixSensitive<- function(num,strFun){
+  auxTep <- matrix(rep("0",num*num),ncol = num) 
+  for(i in 1:num){
+    auxTep[i,i]<-paste(strFun,"Der",sep = "")
+  }
+  auxTep
 }
 
 propagationAdelante <- function(RNA,a0){
-  ##Propagacion -->
-  RNA$An[[1]] <- evalFunc(RNA$Wn[[1]]%*%a0 + RNA$Bn[[1]],RNA$funAct[length(RNA$Wn)])
+    RNA$Nn[[1]]<- RNA$Wn[[1]]%*%a0 + RNA$Bn[[1]]
+    RNA$An[[1]] <- evalFunc(RNA$Nn[[1]],RNA$funAct[1])
+  
   for(i in 2:length(RNA$Wn) ){
-    tmp <- RNA$Wn[[i]]%*%RNA$An[[i-1]] + RNA$Bn[[i]]       
-    RNA$An[[i]]  <- evalFunc(tmp,RNA$funAct[1-length(RNA$Wn)])
+    tmp <- RNA$Wn[[i]]%*%RNA$An[[i-1]] + RNA$Bn[[i]]     
+    RNA$Nn[[i]]  <- tmp
+    RNA$An[[i]]  <- evalFunc(tmp,RNA$funAct[i])
   }  
   RNA
 }
@@ -59,5 +114,14 @@ tansig <- function(x){
   2/(1+exp(-2*x))-1
 }
 
+purelineDer <- function(x){
+  1
+}
 
+logsigDer <- function(x){
+  logsig(x)*(1-logsig(x))
+}
 
+tansigDer <- function(x){
+  1 - tansig(x)^2
+}
